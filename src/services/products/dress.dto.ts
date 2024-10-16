f160ba27ea9bab241e8cc466de5a8f7d318b47bf
LuiFeAn <luisfernandogvv@gmail.com>
2024-10-16 19:57:21 -0300

    refactor: extending base interface

--- src/services/products/dress.dto.ts
+++ src/services/products/dress.dto.ts
@@ -1,14 +1,3 @@
-import { DressStatus } from "@/types/products/product.enums";
+import { IBaseProductCreationInputDto } from "./base.dto";
 
-export type CreateDressInputDTO = {
-    img: File;
-    description: string;
-    price: number;
-    rentable: boolean;
-    purchasable: boolean;
-    status: DressStatus;
-    fabric: string;
-    color: string;
-    model: string;
-    available_for_adjustment: boolean;
-};
+export interface CreateDressInputDTO extends IBaseProductCreationInputDto {}
