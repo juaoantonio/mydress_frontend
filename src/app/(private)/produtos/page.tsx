8b3b2e02089352b65585513f7c4c4942bc9daf05
luifean <luisfernandogvv@gmail.com>
2024-10-23 16:57:33 -0300

    feat: applying product filter component

--- src/app/(private)/produtos/page.tsx
+++ src/app/(private)/produtos/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { ListProductsSection } from "@/app/(private)/produtos/components";
+import { DataTableFiltersProducts } from "./components/filters/data-table-filters.products";
 
-export default async function ListProductsPage() {
+export default function ListProductsPage() {
     return (
         <section className={"mt-2 flex flex-col gap-4"}>
             <div
@@ -12,12 +13,15 @@ export default async function ListProductsPage() {
                 }
             >
                 <h1 className={"self-auto text-xl font-semibold"}>Produtos</h1>
-                <Link href={"/produtos/cadastrar"}>
-                    <Button size={"sm"} className={"gap-1"}>
-                        <Plus size={18} />
-                        Criar Novo Produto
-                    </Button>
-                </Link>
+                <div className="flex items-center gap-2">
+                    <DataTableFiltersProducts />
+                    <Link href={"/produtos/cadastrar"}>
+                        <Button size={"sm"} className={"gap-1"}>
+                            <Plus size={18} />
+                            Criar Novo Produto
+                        </Button>
+                    </Link>
+                </div>
             </div>
 
             <ListProductsSection />
