34774ae353ac40d34e893481bdddbe8cdac9401c
LuiFeAn <luisfernandogvv@gmail.com>
2024-11-07 13:40:56 -0300

    feat: new methods

--- src/services/bookings/booking.service.ts
+++ src/services/bookings/booking.service.ts
@@ -2,11 +2,43 @@ import { axiosClient } from "@/lib/axios.client";
 import { BookingType } from "@/types/booking.types";
 import { GetPaginatedBookingsInputDto } from "@/services/bookings/dtos/get-paginated-bookings.dto";
 import { GetPaginatedOutputDto } from "@/services/types";
+import { CreateBookingInputDto } from "./dtos/create-booking.dto";
 
+export interface IBookingItems {
+    dressIds: string[];
+    clutchIds: string[];
+}
+
+export interface IAdjustments {
+    adjustments: Array<{
+        label: string;
+        description: string;
+        dressId: string;
+    }>;
+}
 export class BookingService {
     async getById(id: string) {
         const response = await axiosClient.get<BookingType>(`/bookings/${id}`);
         return response.data;
+    }
+
+    async create(dto: CreateBookingInputDto) {
+        const response = await axiosClient.post<{
+            bookingId: string;
+        }>(`/bookings`, dto);
+        return response.data;
+    }
+
+    async initProcess(id: string) {
+        await axiosClient.patch(`/bookings/${id}/init`);
+    }
+
+    async bookingItems(id: string, data: IBookingItems) {
+        await axiosClient.patch(`/bookings/${id}/items`, data);
+    }
+
+    async adjustments(id: string, data: IAdjustments) {
+        await axiosClient.patch(`/bookings/${id}/adjustments`, data);
     }
 
     async getPaginated(
