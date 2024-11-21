6fccce8b3ef8808e96916800f9da5cc9af502eea
João Barbosa <joaobarbosadev@gmail.com>
2024-11-21 10:53:52 -0300

    feat: add pwa support

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: "My Dress Pro Suite - Sistema de Gerencimento de Aluguel de Vestidos",
+        short_name: "my-dress-pro-suite",
+        description: "Sistema de Gerencimento de Aluguel de Vestidos",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+            {
+                src: "/web-app-manifest-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "maskable",
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable",
+            },
+        ],
+    };
+}
